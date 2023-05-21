de1a600059939bcd3d22ddea58de39d802ccd3ca
Hans Schouten <hans@falcosolutions.nl>
2023-05-21 11:33:07 +0200

    Add HasError and AlertSuccess

--- src/boot/main.js
+++ src/boot/main.js
@@ -1,9 +1,15 @@
 import { boot } from 'quasar/wrappers'
 import { createPinia } from 'pinia'
+import { HasError } from 'vform/src/components/bootstrap5'
+import AlertSuccess from 'components/AlertSuccess.vue'
 
 export default boot(async ({ app, router, store }) => {
   const pinia = createPinia()
   app.use(pinia)
+
+  // register global components
+  app.component(HasError.name, HasError)
+  app.component(AlertSuccess.name, AlertSuccess)
 
   window.appName = import.meta.env.VITE_APP_NAME
 })
---
+++ src/components/AlertSuccess.vue
@@ -0,0 +1,36 @@
+<template>
+  <q-banner v-if="form.successful" inline-actions class="text-white bg-green">
+    {{ message }}
+    <template v-if="dismissible" v-slot:action>
+      <q-btn flat color="white" label="Dismiss" @click="dismiss" />
+    </template>
+  </q-banner>
+</template>
+
+<script>
+export default {
+  name: 'AlertSuccess',
+  props: {
+    form: {
+      type: Object,
+      required: true,
+    },
+    message: {
+      type: String,
+      default: '',
+    },
+    dismissible: {
+      type: Boolean,
+      default: true,
+    },
+  },
+
+  methods: {
+    dismiss() {
+      if (this.dismissible) {
+        this.form.clear()
+      }
+    },
+  },
+}
+</script>
